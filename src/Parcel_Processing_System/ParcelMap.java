8d530d2d912ec38e24f6b85777e207ca9fe92413
FairuzIqbal11 <fi22aaj@herts.ac.uk>
2024-12-22 21:19:39 +0000

    Completion of ParcelMap class

--- src/Parcel_Processing_System/ParcelMap.java
+++ src/Parcel_Processing_System/ParcelMap.java
@@ -1,2 +1,35 @@
-package Parcel_Processing_System;public class ParcelMap {
+package Parcel_Processing_System;
+
+import java.util.HashMap;
+import java.util.Map;
+public class ParcelMap {
+    private Map<String, Parcel> parcelMap;
+
+    //Constructor
+    public ParcelMap(){
+        this.parcelMap = new HashMap<>();
+    }
+
+    //Add a parcel to the map
+    public void addParcel (Parcel parcel){
+        parcelMap.put(parcel.getParcelID(), parcel);
+    }
+
+    // To retrieve a parcel by ID
+    public Parcel getParcel (String parcelID){
+        return parcelMap.get(parcelID);
+    }
+
+    //To remove a parcel with its ID
+    public Parcel removeParcel(String parcelID){
+        return parcelMap.remove(parcelID);
+    }
+
+    //To display all parcel
+    public void displayParcels (){
+        for (Parcel parcel: parcelMap.values()){
+            System.out.println (parcel);
+        }
+    }
+
 }

deb4c11537b5fe91c0ae8df2fc96256d25d06638
FairuzIqbal11 <fi22aaj@herts.ac.uk>
2024-12-22 12:54:05 +0000

    Completion of Parcel class

--- src/Parcel_Processing_System/Parcel.java
+++ src/Parcel_Processing_System/Parcel.java
@@ -1,4 +1,70 @@
 package Parcel_Processing_System;
 
 public class Parcel {
+    //fields
+    private String parcelID;
+    private int daysInDepot;
+    private double weight;
+    private String dimensions;
+    private String status;
+
+    //Constructor
+    public Parcel (String parcelID, int daysInDepot, double weight, String dimensions,String status){
+        this.parcelID = parcelID;
+        this.daysInDepot = daysInDepot;
+        this.weight = weight;
+        this.dimensions = dimensions;
+        this.status = status;
+    }
+    //Getters and Setters
+    public String getParcelID(){
+        return parcelID;
+    }
+    public void setParcelID(String parcelID) {
+        this.parcelID = parcelID;
+    }
+
+    public int getDaysInDepot() {
+        return daysInDepot;
+    }
+    public void setDaysInDepot(int daysInDepot) {
+        this.daysInDepot = daysInDepot;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+   // toString method
+
+   public String toString() {
+       return "Parcel: ID= " + parcelID + ", DaysInDepot= " + daysInDepot +
+               ", Weight= " + weight + ", Dimensions= " + dimensions +
+               ", Status= " + status;
+   }
+
+
+
+
 }
+
+
